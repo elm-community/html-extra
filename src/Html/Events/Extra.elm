e207c2112477e0c234726592401d7b440389cc26
Rehno Lindeque <errantkid@gmail.com>
2015-02-16 14:39:21 +0200

    Add TODO's for DOM3 keyboard event decoders

--- src/Html/Events/Extra.elm
+++ src/Html/Events/Extra.elm
@@ -2,6 +2,9 @@ module Html.Events.Extra where
 {-| Additional event handlers for html.
 
 # Event decoders
+* TODO: `key`
+* TODO: `code`
+* TODO: `KeyEvent`, `keyEvent`
 @docs charCode
 
 # Typed event decoders
@@ -16,6 +19,36 @@ import Json.Decode ((:=))
 import Result
 import String
 import Maybe
+
+-- TODO
+-- {-| Decode the key that was pressed.
+-- The key attribute is intended for users who are interested in the meaning of the key being pressed, taking into account the current keyboard layout.
+--
+-- * If there exists an appropriate character in the [key values set](http://www.w3.org/TR/DOM-Level-3-Events-key/#key-value-tables), this will be the result. See also [MDN key values](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent.key#Key_values).
+-- * If the value is has a printed representation, it will be a non-empty Unicode character string consisting of the char value.
+-- * If more than one key is being pressed and the key combination includes modifier keys (e.g. `Control + a`), then the key value will still consist of the printable char value with no modifier keys except for 'Shift' and 'AltGr' applied.
+-- * Otherwise the value will be `"Unidentified"`
+--
+-- Note that `keyCode`, `charCode` and `which` are all being deprecated. You should avoid using these in favour of `key` and `code`.
+-- Google Chrome and Safari currently support this as `keyIdentifier` which is defined in the old draft of DOM Level 3 Events.
+--
+-- -}
+-- key : Json.Decoder String
+-- key = Json.oneOf [ "key" := string, "keyIdentifier" := string ]
+
+-- TODO: Waiting for proper support in chrome & safari
+-- {-| Return a string identifying the key that was pressed.
+-- `keyCode`, `charCode` and `which` are all being deprecated. You should avoid using these in favour of `key` and `code`.
+-- See [KeyboardEvent.keyCode](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent.keyCode).
+-- -}
+-- code : Json.Decoder String
+-- code =
+--     ("code" := string)
+
+-- TODO: Complete keyboard event
+-- keyEvent : Json.Decoder KeyEvent
+-- keyEvent =
+--     Json.oneOf [ ("keyCode" := int)
 
 {-| Character code for key board events.
 This is being deprecated, but support for DOM3 Keyboard events is not yet present in most browsers.
