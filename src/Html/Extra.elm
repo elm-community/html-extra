16d221260ce791a20dc70d7e752d40ab028693d2
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2016-05-17 10:26:30 +0200

    expand documentation of Html.Extra.static

--- src/Html/Extra.elm
+++ src/Html/Extra.elm
@@ -22,6 +22,10 @@ in `Html Never` tells us that the html has no event handlers attached,
 it will not generate any messages. We may want to embed such static
 html into arbitrary views, while using types to enforce the
 staticness. That is what this function provides.
+
+*Note:* To call this function, the argument need not be literally of type
+`Html Never`. It suffices if it is a fully polymorphic (in the message type)
+`Html` value. For example, this works: `static (Html.text "abcdef")`.
 -}
 static : Html Never -> Html msg
 static =
