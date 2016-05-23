d49693781c30ed602d4586a2dae8ef1c213777a5
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2016-05-23 21:02:16 +0200

    Add Html.Extra.static (#6)

---
+++ src/Html/Extra.elm
@@ -0,0 +1,32 @@
+module Html.Extra
+    exposing
+        ( static
+        )
+
+{-| Convenience functionality on
+[`Html`](http://package.elm-lang.org/packages/elm-lang/html/latest/Html#Html)
+
+@docs static
+-}
+
+import Basics.Extra
+import Html exposing (Html)
+import Html.App
+
+
+{-| Embedding static html.
+
+The type argument
+[`Never`](http://package.elm-lang.org/packages/elm-lang/core/latest/Basics#Never)
+in `Html Never` tells us that the html has no event handlers attached,
+it will not generate any messages. We may want to embed such static
+html into arbitrary views, while using types to enforce the
+staticness. That is what this function provides.
+
+*Note:* To call this function, the argument need not be literally of type
+`Html Never`. It suffices if it is a fully polymorphic (in the message type)
+`Html` value. For example, this works: `static (Html.text "abcdef")`.
+-}
+static : Html Never -> Html msg
+static =
+    Html.App.map Basics.Extra.never
