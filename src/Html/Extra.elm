b7fd06a8f24f8d3835a288114719af45b8eb9982
Chadtech <chadstearns1@gmail.com>
2018-07-21 16:43:00 +0200

    View if functions

--- src/Html/Extra.elm
+++ src/Html/Extra.elm
@@ -1,12 +1,15 @@
 module Html.Extra
     exposing
         ( static
+        , viewIf
+        , viewIfLazy
         )
 
 {-| Convenience functionality on
 [`Html`](http://package.elm-lang.org/packages/elm-lang/html/latest/Html#Html)
 
-@docs static
+@docs viewIf, viewIflazy, static
+
 -}
 
 import Html exposing (Html)
@@ -21,10 +24,53 @@ it will not generate any messages. We may want to embed such static
 html into arbitrary views, while using types to enforce the
 staticness. That is what this function provides.
 
-*Note:* To call this function, the argument need not be literally of type
+_Note:_ To call this function, the argument need not be literally of type
 `Html Never`. It suffices if it is a fully polymorphic (in the message type)
 `Html` value. For example, this works: `static (Html.text "abcdef")`.
+
 -}
 static : Html Never -> Html msg
 static =
     Html.map never
+
+
+{-| A function to only render html under a certain condition
+
+    fieldView : Model -> Html Msg
+    fieldView model =
+        div
+            []
+            [ fieldInput model
+            , viewIf
+                (not <| List.isEmpty model.errors)
+                errorsView
+            ]
+
+-}
+viewIf : Bool -> Html msg -> Html msg
+viewIf condition html =
+    if condition then
+        html
+    else
+        Html.text ""
+
+
+{-| Just like `viewIf` except its more performant. In viewIf, the html is always evaluated, even if its not rendered. `viewIfLazy` only evaluates your view function if it needs to. The trade off is your view function needs to accept a unit type (`()`) as its final parameter
+
+    fieldView : Model -> Html Msg
+    fieldView model =
+        div
+            []
+            [ fieldInput model
+            , viewIf
+                (not <| List.isEmpty model.errors)
+                errorsView
+            ]
+
+-}
+viewIfLazy : Bool -> (() -> Html msg) -> Html msg
+viewIfLazy condition htmlF =
+    if condition then
+        htmlF ()
+    else
+        Html.text ""
